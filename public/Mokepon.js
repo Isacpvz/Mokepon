cad4098a8c13114c7c7cb8f3d9d90e084e38ed1b
Isacpvz <josueisaclermacarrera@gmail.com>
2025-04-06 12:47:02 -0600

    Aws3

--- public/Mokepon.js
+++ public/Mokepon.js
@@ -153,7 +153,7 @@ sectionReiniciar.style.display= 'none'
 unirseAlJuego()
 
 function unirseAlJuego(){
-    fetch("http://172.31.80.24:8080/unirse")
+    fetch("http://ec2-3-87-34-171.compute-1.amazonaws.com:8080/unirse")
     .then(function (res){
         if(res.ok){
             res.text()
@@ -193,7 +193,7 @@ function seleccionarMascotaJugador(){
 }
 
 function seleccionarMokepon(mascotaJugador){
-    fetch(`http://172.31.80.24:8080/mokepon/${jugadorId}`,{
+    fetch(`http://ec2-3-87-34-171.compute-1.amazonaws.com:8080/mokepon/${jugadorId}`,{
         method: "post",
         headers: {
             "Content-type": "application/json"
@@ -255,7 +255,7 @@ function secuenciaAtaque(){
 }
 
 function enviarAtaques(){
-    fetch(`http://172.31.80.24:8080/mokepon/${jugadorId}/ataques`, {
+    fetch(`http://ec2-3-87-34-171.compute-1.amazonaws.com:8080/mokepon/${jugadorId}/ataques`, {
         method: "post",
         headers:{
             "Content-Type": "application/json"
@@ -269,7 +269,7 @@ function enviarAtaques(){
 }
 
 function obtenerAtaques(){
-    fetch(`http://172.31.80.24:8080/mokepon/${enemigoId}/ataques`)
+    fetch(`http://ec2-3-87-34-171.compute-1.amazonaws.com:8080/mokepon/${enemigoId}/ataques`)
     .then(function (res){
         if (res.ok){
             res.json()
@@ -408,7 +408,7 @@ function pintarCanva(){
 }
 
 function enviarPosicion(x,y){
-    fetch(`http://172.31.80.24:8080/mokepon/${jugadorId}/posicion`, {
+    fetch(`http://ec2-3-87-34-171.compute-1.amazonaws.com:8080/mokepon/${jugadorId}/posicion`, {
         method: "post",
         headers: {
             "Content-Type": "application/json"
